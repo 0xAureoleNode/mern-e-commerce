96824b202e9e23fff563368b4db75b408af31b35
”0xAureoleNode“ <aureolenode@gmail.com>
2022-12-20 21:31:05 +0800

    added 1: productdetailspage - useParams / id 2: 通配符路由，用于处理不存在时页面的信息返回

--- frontend/src/pages/ProductDetailsPage.js
+++ frontend/src/pages/ProductDetailsPage.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const ProductDetailsPage = () => {
+  const { id } = useParams();
+  console.log(id);
   return <div>This is a ProductDetailsPage</div>;
 };
 
