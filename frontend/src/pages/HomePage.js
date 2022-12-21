a2dc6bf0efd2da519b8b685d6bedbfc6ce5835c6
”0xAureoleNode“ <aureolenode@gmail.com>
2022-12-21 22:00:45 +0800

    Create Carousel and Card Components

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import ProductCarouselComponent from '../components/ProductCarouselComponent';
+import CategoryCardComponent from '../components/CategoryCardComponent';
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <ProductCarouselComponent />
+      <CategoryCardComponent />
+    </div>
+  );
 };
 
 export default HomePage;
