65c8d91420dc3b2df63de783805574e60f201aea
”0xAureoleNode“ <aureolenode@gmail.com>
2022-12-21 22:41:44 +0800

    customize the home page

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -2,11 +2,29 @@ import React from 'react';
 import ProductCarouselComponent from '../components/ProductCarouselComponent';
 import CategoryCardComponent from '../components/CategoryCardComponent';
 
+import { Row, Container } from 'react-bootstrap';
+
 const HomePage = () => {
+  const categories = [
+    'Tablets',
+    'Monitors',
+    'Games',
+    'Printers',
+    'Software',
+    'Cameras',
+    'Books',
+    'Videos',
+  ];
   return (
     <div>
       <ProductCarouselComponent />
-      <CategoryCardComponent />
+      <Container>
+        <Row xs={1} md={2} className="g-4 mt-5">
+          {categories.map((category, idx) => (
+            <CategoryCardComponent key={idx} category={category} idx={idx} />
+          ))}
+        </Row>
+      </Container>
     </div>
   );
 };
