3fa0e10649bba11c362cab13bfc5cae2a65bacfd
”0xAureoleNode“ <aureolenode@gmail.com>
2023-01-03 17:16:56 +0800

    prepare detail page

--- frontend/src/pages/user/UserOrderDetailsPage.js
+++ frontend/src/pages/user/UserOrderDetailsPage.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const UserOrderDetailsPage = () => {
-  return <div>UserOrderDetailsPage</div>;
+  return (
+    <Container>
+      <Row className="mt-4">
+        <h1>Order Details</h1>
+        <Col md={8}>
+          <br />
+          <Row>
+            <Col md={6}>
+              <h2>Shipping</h2>
+            </Col>
+            <Col md={6}>
+              <h2>Payment method</h2>
+            </Col>
+          </Row>
+        </Col>
+        <Col md={4}>
+          <h3>Order Summery</h3>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default UserOrderDetailsPage;
