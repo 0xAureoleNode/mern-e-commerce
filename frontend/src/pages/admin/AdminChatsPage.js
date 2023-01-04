a75429e6aea7401328686ac511f21fef2b80a52f
”0xAureoleNode“ <aureolenode@gmail.com>
2023-01-04 16:09:17 +0800

    Create Chats Page

--- frontend/src/pages/admin/AdminChatsPage.js
+++ frontend/src/pages/admin/AdminChatsPage.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Row, Col } from 'react-bootstrap';
+import AdminLinkComponent from '../../components/admin/AdminLinkComponent';
+import AdminChatRoomComponent from '../../components/admin/AdminChatRoomComponent';
 
 const AdminChatsPage = () => {
-  return <div>AdminChatsPage</div>;
+  return (
+    <Row className="m-5">
+      <Col md={2}>
+        <AdminLinkComponent />
+      </Col>
+      <Col md={10}>
+        <AdminChatRoomComponent />
+      </Col>
+    </Row>
+  );
 };
 
 export default AdminChatsPage;
